5fd39a9d2da367e2d7cea70264d391861d62b0cd
Raphael Almeida <raphael.almeida@dataprev.gov.br>
2020-01-22 16:37:08 -0200

    .

--- src/main/java/br/com/samayah/infra/BusinessException.java
+++ src/main/java/br/com/samayah/infra/BusinessException.java
@@ -1,9 +1,11 @@
 package br.com.samayah.infra;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 
-@Data
+@Data 
+@EqualsAndHashCode(callSuper = false)
 public class BusinessException extends Exception {
 
 	private static final long serialVersionUID = -1703407560441026366L;
